29d3c9199f91bcd3fa2e352b555eac29feabcf34
ayush13344 <147728663+ayush13344@users.noreply.github.com>
2025-03-27 17:43:01 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,47 @@
-import './App.css'
-import axios from "axios"
-export default function App() {
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
+
+import Signup from './components/Signup';
+import Login from "./components/Login";
+
+import Mainhero from "./components/home/Mainhero";
+import Navbar from "./components/Navbar";
+import Mindash from "./components/Dash/Mindash";
+import Res from "./components/Dash/res";
+import Dash from "./components/Dash/DAsh";
+import ResourcesPage from "./components/Dash/reso/ResoPage";
+
+function Layout() {
+  const location = useLocation();
+  const hideElementsOnRoutes = ["/signup","/login"]; 
+
   return (
     <>
-         <div>App</div>
+      {!hideElementsOnRoutes.includes(location.pathname) && <Navbar />}
+      
+      <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Mainhero />} />
+      <Route path="/dashboard" element={<Dash />} />
+      <Route path="/res" element={<Res/>}/>
+      <Route path="/resource" element={<ResourcesPage/>}/>
+      <Route path="/dash" element={<Dash />} />
+    </Routes>
+      
+   
     </>
+  );
+}
+
+function App() {
+
+  return (
+    <BrowserRouter>
+     
+    <Layout/>
+    </BrowserRouter>
   )
 }
+
+export default App
